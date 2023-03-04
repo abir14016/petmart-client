d7a6fd664010d11aeec52a61f9e95305f6fd705d
abir14016 <abir14016@gmail.com>
2023-03-05 04:08:32 +0600

    route setup and background image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
+import backgroundImage from "./assets/background/scattered-forcefields.svg";
 
 function App() {
+  const background = {
+    width: '100%',
+    height: '100vh',
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundAttachment: 'fixed'
+  };
   return (
-    <div className="App">
-      <p className=' text-purple-700 text-3xl font-bold'>Tailwind css installed</p>
-      <button>Tailwind</button>
-      <button className='btn btn-outline'>daisyUI</button>
-      <button className='btn btn-primary'>hello</button>
-      <button className='btn btn-secondary'>hello</button>
-      <button className='btn btn-accent'>hello</button>
+    <div className="App" style={background}>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
     </div>
 
   );
